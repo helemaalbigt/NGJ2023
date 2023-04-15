4d03084de0c9880f635363832ffa7030d5b83e10
VANBO\vanbo <van.bouwel.thomas@gmail.com>
2023-04-15 10:28:48 +0200

    WIP calibration

--- Assets/#Project/Scripts/BootCalibration.cs
+++ Assets/#Project/Scripts/BootCalibration.cs
@@ -1,14 +1,73 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Rowhouse;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class BootCalibration : MonoBehaviour
 {
-    public bool _triggerPressed;
+    [Header("Anchors")]
+    public Transform _shoeLeft;
+    public Transform _shoeRight;
+    public Transform _shoeLeftAnchor;
+    public Transform _shoeRightAnchor;
     
+    [Header("Passthrough")]
+    public OVRPassthroughLayer _passthroughLayer;
+    public Camera _mainCamera;
+    public GameObject _calibrationWrapper;
+    public GameObject _gameWrapper;
+    
+    private bool _calibrating;
+    private bool _triggersHeld;
+
+    private void Start() {
+        _calibrating = true;
+        SetCalibrationView();
+    }
+
     void Update()
     {
-        _triggerPressed = InputManager.I.Trigger(Hand.right);
+        if (InputManager.I.Trigger(Hand.right) && InputManager.I.Trigger(Hand.left) && !_calibrating && !_triggersHeld) {
+            _calibrating = true;
+            _triggersHeld = true;
+
+            SetCalibrationView();
+        }
+        
+        if (InputManager.I.Trigger(Hand.right) && InputManager.I.Trigger(Hand.left) && _calibrating && !_triggersHeld) {
+            _calibrating = false;
+            _triggersHeld = true;
+            
+            _shoeLeft.position = _shoeLeftAnchor.position;
+            _shoeLeft.rotation = _shoeLeftAnchor.rotation;
+            _shoeRight.position = _shoeRightAnchor.position;
+            _shoeRight.rotation = _shoeRightAnchor.rotation;
+            
+            SetGameView();
+        }
+        
+        if(_triggersHeld && !InputManager.I.Trigger(Hand.right) && !InputManager.I.Trigger(Hand.left)) {
+            _triggersHeld = false;
+        }
+    }
+
+    private void SetCalibrationView() {
+        _passthroughLayer.enabled = true;
+        _mainCamera.clearFlags = CameraClearFlags.SolidColor;
+        _calibrationWrapper.SetActive(true);
+        _gameWrapper.SetActive(false);
+        _shoeLeft.gameObject.SetActive(false);
+        _shoeRight.gameObject.SetActive(false);
+    }
+    
+    private void SetGameView() {
+        _passthroughLayer.enabled = false;
+        _mainCamera.clearFlags = CameraClearFlags.Skybox;
+        _calibrationWrapper.SetActive(false);
+        _gameWrapper.SetActive(true);
+        _shoeLeft.gameObject.SetActive(true);
+        _shoeRight.gameObject.SetActive(true);
     }
 }

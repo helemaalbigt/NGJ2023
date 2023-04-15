a162fa6061e07247e9fd97960210d70848977184
Antonia Forster <52669070+GlitchesGetStitches@users.noreply.github.com>
2023-04-15 01:50:51 +0100

    Ant can now grab fruit (tagged as "grabbable")
    Currently implemented crudely using collider and parenting (when ant collides with fruit, fruit is parented to ant). Tomorrow will rebuild using nicer system of raycast from ant, and fruit having a "follow" script (set transform to transform of ant carrying it, plus some offset)

--- Assets/AntController.cs
+++ Assets/AntController.cs
@@ -4,6 +4,12 @@ public class AntController : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public float rotationSpeed = 100f;
+    public float raycastDistance = 1f;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.forward * raycastDistance);
+    }
 
     void Update()
     {
@@ -15,5 +21,27 @@ public class AntController : MonoBehaviour
 
         // Rotate the player on the Y axis based on input
         transform.Rotate(Vector3.up, horizontal * rotationSpeed * Time.deltaTime);
+        if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, raycastDistance))
+        {
+            Debug.Log(hit.collider.gameObject.name);
+        }
+    }
+
+    //TODO:
+    //When raycast hits a grabbable object, switch a boolean GrabbedFruit to true
+    //When this boolean is true, the fruit will set its transform to the transform of the ant plus some offset
+    //(When ants grabs fruit, it checks the difference between its transform and fruit transform, and that is the offset)
+    //Then remove this collider system
+
+    //Grab food or other grabbable objects
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collided with something");
+
+        if (other.CompareTag("Grabbable"))
+        {
+            Debug.Log("Collided with grabbable object");
+            other.transform.SetParent(transform, true);
+        }
     }
 }
---
+++ Assets/GrabbableObject.cs
@@ -0,0 +1,16 @@
+//using UnityEngine;
+
+//public class GrabbableObject : MonoBehaviour
+//{
+//    void Update()
+//    {
+//        if (Vector3.Distance(transform.position, playerTransform.position) < .2f)
+//        {
+//            transform.parent = playerTransform;
+//        }
+//        else
+//        {
+//            transform.parent = null;
+//        }
+//    }
+//}
